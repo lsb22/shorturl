8ec4fa86c88639851c0cd097a0297755d3f93b76
lsb <likith15600@gmail.com>
2025-04-10 15:02:34 +0530

    Removed url redirection code

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -7,30 +7,30 @@ homeRouter.get("/", (req, res) => {
   res.send("Welcome to url shortner");
 });
 
-homeRouter.get("/:surl", async (req, res) => {
-  const { surl } = req.params;
-  try {
-    const url = await urlModel.find({ shortUrl: surl });
-    if (url.length == 0) {
-      return res.status(401).json({ message: "Url doesn't exist" });
-    }
-    urlModel
-      .updateOne(
-        {
-          shortUrl: surl,
-        },
-        {
-          $set: {
-            clicks: url[0].clicks + 1,
-          },
-        }
-      )
-      .then(() => console.log("clicks updated successfully"))
-      .catch((err) => console.log(err));
-    res.redirect(url[0].orgUrl);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
+// homeRouter.get("/:surl", async (req, res) => {
+//   const { surl } = req.params;
+//   try {
+//     const url = await urlModel.find({ shortUrl: surl });
+//     if (url.length == 0) {
+//       return res.status(401).json({ message: "Url doesn't exist" });
+//     }
+//     urlModel
+//       .updateOne(
+//         {
+//           shortUrl: surl,
+//         },
+//         {
+//           $set: {
+//             clicks: url[0].clicks + 1,
+//           },
+//         }
+//       )
+//       .then(() => console.log("clicks updated successfully"))
+//       .catch((err) => console.log(err));
+//     res.redirect(url[0].orgUrl);
+//   } catch (error) {
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
 
 export default homeRouter;
